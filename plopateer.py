1093c736ad2f9c1074dd029db28eaeb169222580
Logan Moore <lmooore@gmail.com>
2016-06-23 16:11:52 -0400

    Initial code commit

---
+++ plopateer.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello World!'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        pass # login
+    else:
+        pass # login form
+
+
+@app.route('/post/<int:post_id>')
+def posts(post_id):
+    return "Post #{}".format(post_id)
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return "User {}".format(username)
+
+
+if __name__ == '__main__':
+    app.run()
