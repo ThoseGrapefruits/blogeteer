55a73183321eebe3dd41c6800d9706509736df06
Logan Moore <lmooore@gmail.com>
2016-06-27 14:52:31 -0400

    Added initdb confirmation

--- runserver.sh
+++ runserver.sh
@@ -2,7 +2,17 @@
 . ./load_requirements.sh
 
 if [ "$1" == "initdb" ]; then
-  FLASK_APP=plopateer.py flask initdb
+  echo " Are you sure you would like to initialise the database?"
+  read -n1 -p " This will erase all data. [y/N] " response
+
+  if [ -z $response ]; then
+    echo "Database initialisation cancelled."
+  fi
+
+  case "$response" in 
+       [yY]) FLASK_APP=plopateer.py flask initdb;;
+          ?) echo "Database initialisation cancelled.";;
+  esac
 fi
 
 FLASK_APP=plopateer.py flask run
