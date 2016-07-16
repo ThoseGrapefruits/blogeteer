d32e2ec51bbe34e6b367873f56e6bfb55e6e2c29
Logan Moore <logan@tmoore.io>
2016-07-16 00:46:03 -0400

    Humble beginnings of testing

---
+++ test_plopateer.py
@@ -0,0 +1,24 @@
+import plopateer
+
+
+def test_slugify():
+    input_result = {
+        'banana man': 'banana-man',
+        'a%%%%b': 'a-b',
+        'a--b': 'a-b',
+        'hey--hey----hey$*(#%&@#': 'hey-hey-hey'
+    }
+    for (key, value) in input_result.items():
+        assert plopateer.slugify(key) == value
+
+
+# ------------------------------------------------------------------------------------------------
+
+# MAIN
+
+def run_tests():
+    test_slugify()
+
+
+if __name__ == '__main__':
+    run_tests()
